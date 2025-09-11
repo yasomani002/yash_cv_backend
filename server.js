84215c4d9fea0e7f81df96c38c2b28b7cfc99dbe
Yash Somani <yashsomani231@gmail.com>
2025-09-11 18:23:11 +0530

    add cors

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const app = express();
 const router = express.Router()
 const userRoute = require('./routes/userRoute..js');
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 
 app.use(cookieParser());
 app.use(express.json()); // Middleware to parse JSON request body
 require('dotenv').config();
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://yash-cv-frontend.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.use(router);
 app.listen(process.env.LOCAL_PORT, () => {
